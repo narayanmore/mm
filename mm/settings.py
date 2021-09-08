00e98fa18fbd5faa9549187d16bae7a1088042bd
narayanmore <narayanmore2019@gmail.com>
2021-09-08 14:45:27 +0530

    more1

--- mm/settings.py
+++ mm/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-uhhry##2f*&o8mtsb+)+ze8_k*@(ih3pyk=q2o!v_khx!ua6%d'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
