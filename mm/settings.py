177bdf1c6850118743e523ec1ed95ad2442c97d0
narayanmore <narayanmore2019@gmail.com>
2021-09-09 17:25:36 +0530

    css

--- mm/settings.py
+++ mm/settings.py
@@ -28,6 +28,7 @@ SECRET_KEY = 'django-insecure-uhhry##2f*&o8mtsb+)+ze8_k*@(ih3pyk=q2o!v_khx!ua6%d
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
+
 ALLOWED_HOSTS = ["*"]
 
 
